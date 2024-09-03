87d1546d4264929bd2cbdc6ea59dafd1dba054dd
Abraham <abrahamnhier97@gmail.com>
2024-09-03 16:32:24 +0300

    fetching all issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const api_url = 'http://localhost:5000/issues';
 
 function App() {
+  const [issues, setIssues] = useState([]);
+  
+  
+  //fetch issues
+  useEffect(() => {
+  const fetchIssues = async() => {
+    try {
+      const response = await axios.get(api_url);
+      console.log(response.data);
+      setIssues([...issues, response.data]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+    fetchIssues();
+  }, [issues])
   return (
     <div className="App">
       <h1 className="text-center">Welcome!</h1>

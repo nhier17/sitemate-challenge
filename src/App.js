f351ea9b78834d2c5b554345e136e3d2a96a57cb
Abraham <abrahamnhier97@gmail.com>
2024-09-03 16:45:25 +0300

    displaying the issues

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   
   
   //fetch issues
-  useEffect(() => {
+ 
   const fetchIssues = async() => {
     try {
       const response = await axios.get(api_url);
@@ -18,12 +18,28 @@ function App() {
       console.error(error);
     }
   }
-
+  useEffect(() => {
     fetchIssues();
-  }, [issues])
+  }, [])
   return (
-    <div className="App">
-      <h1 className="text-center">Welcome!</h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Issue Tracker</h1>
+      <div className="w-full max-w-md">
+        <h2 className="text-xl font-semibold mb-4">Issues</h2>
+        <ul className="space-y-4">
+          {issues.map((issue) => (
+            <li
+            key={issue.id}
+            className="bg-white p-4 rounded-lg shadow-md flex justify-between items-start"
+            >
+              <div>
+                <h3 className="text-lg font-bold text-gray-700">{issues.title}</h3>
+                 <p className="text-gray-700">{issues.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
